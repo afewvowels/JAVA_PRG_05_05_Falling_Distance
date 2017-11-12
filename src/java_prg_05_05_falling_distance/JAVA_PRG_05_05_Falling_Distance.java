bd2ab8c715ddca8f2a58039740fb9ecd7c4677a9
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 14:34:41 -0500

    Prompt written.

---
+++ src/java_prg_05_05_falling_distance/JAVA_PRG_05_05_Falling_Distance.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_05_falling_distance;
+
+/**
+ *
+ * @author bluebackdev
+ * When an object is falling because of gravity, the following formula can be
+ * used to determine the distance the object falls in a specific time period:
+ * 
+ *      d = 1/2 * gt^2
+ * 
+ * The variables in the formula are as follows: d is the distance in meters,
+ * g is 9.8, and t is the amount of time, in seconds, that the object has been
+ * falling.
+ * 
+ * Write a method named fallingDistance that accepts an object's falling time
+ * (in seconds) as an argument. The method should return the distance, in
+ * meters, that the object has fallen during that time interval. Demonstrate
+ * the method by calling it in a loop that passes the values 1 through 10 as
+ * arguments, and displays the return value.
+ */
+public class JAVA_PRG_05_05_Falling_Distance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
